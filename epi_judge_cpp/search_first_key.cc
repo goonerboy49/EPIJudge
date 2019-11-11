2dccdef6361eccc25888a2d815ab9a4f2cbbed2e
Karthik Rao <nmkartikrao@gmail.com>
2019-11-10 17:37:41 -0800

    Binary search first index equal to an element

--- epi_judge_cpp/search_first_key.cc
+++ epi_judge_cpp/search_first_key.cc
@@ -3,8 +3,25 @@
 using std::vector;
 
 int SearchFirstOfK(const vector<int>& A, int k) {
-  // TODO - you fill in here.
-  return 0;
+  int left = 0;
+  int right = A.size() - 1;
+  int result = -1;
+
+  while(left <= right) {
+    int mid = left + (right - left) / 2;
+
+    auto midElem = A[mid];
+    if (midElem < k) {
+      left = mid+1;
+    } else if (midElem == k) {
+      result = mid;
+      right = mid-1;
+    } else {
+      right = mid-1;
+    }
+  }
+
+  return result;
 }
 
 int main(int argc, char* argv[]) {
