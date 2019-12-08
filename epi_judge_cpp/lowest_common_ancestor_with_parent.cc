5c960e8c34105e07931abb59de4fa4fb92781420
Karthik Rao <nmkartikrao@gmail.com>
2019-12-08 12:19:10 -0800

    Lowest common ancestor with parent

--- epi_judge_cpp/lowest_common_ancestor_with_parent.cc
+++ epi_judge_cpp/lowest_common_ancestor_with_parent.cc
@@ -4,10 +4,38 @@
 #include "test_framework/test_failure.h"
 #include "test_framework/timed_executor.h"
 
+int GetDepth(BinaryTreeNode<int> *node) {
+  int depth = 0;
+  while(node) {
+    node = node->parent;
+    ++depth;
+  }
+  return depth;
+}
+
 BinaryTreeNode<int>* LCA(const unique_ptr<BinaryTreeNode<int>>& node0,
                          const unique_ptr<BinaryTreeNode<int>>& node1) {
-  // TODO - you fill in here.
-  return nullptr;
+  int node0Depth = GetDepth(node0.get());
+  int node1Depth = GetDepth(node1.get());
+
+  BinaryTreeNode<int> *node0Ptr = node0.get();
+  BinaryTreeNode<int> *node1Ptr = node1.get();
+  if (node1Depth > node0Depth) {
+    std::swap(node0Ptr, node1Ptr);
+  }
+
+  int diff = abs(node0Depth - node1Depth);
+  while(diff) {
+    node0Ptr = node0Ptr->parent;
+    --diff;
+  }
+
+  while(node0Ptr != node1Ptr) {
+    node0Ptr = node0Ptr->parent;
+    node1Ptr = node1Ptr->parent;
+  }
+
+  return node0Ptr;
 }
 int LcaWrapper(TimedExecutor& executor,
                const unique_ptr<BinaryTreeNode<int>>& tree, int key0,
