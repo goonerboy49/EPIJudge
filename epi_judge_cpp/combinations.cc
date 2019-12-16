090fb5020ea71bf88ed28ef7a1144320b9d7eee5
goonerboy49 <nmkartikrao@gmail.com>
2019-12-16 14:39:20 -0800

    Combinations

--- epi_judge_cpp/combinations.cc
+++ epi_judge_cpp/combinations.cc
@@ -2,9 +2,27 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
+void RecurseCombinations(int idx, int n, int k, vector<int> *currAns, vector<vector<int>>& ans) 
+{
+  if (currAns->size() == k) {
+    ans.emplace_back(*currAns);
+    return;
+  }
+
+  const int num_remaining = k - currAns->size();
+  for(int i = idx; i <= n && num_remaining <= n-i+1; i++) {
+    currAns->push_back(i);
+    RecurseCombinations(i+1, n, k, currAns, ans);
+    currAns->pop_back();
+  }
+}
+
 vector<vector<int>> Combinations(int n, int k) {
-  // TODO - you fill in here.
-  return {};
+  vector<vector<int>> ans;
+  auto currAns = std::make_unique<vector<int>>();
+
+  RecurseCombinations(1, n, k, currAns.get(), ans);
+  return ans;
 }
 
 int main(int argc, char* argv[]) {
