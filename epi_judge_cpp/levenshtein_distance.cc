0e84e495c0de71d3b9b9d1b1906d532f28ca33f4
goonerboy49 <nmkartikrao@gmail.com>
2019-12-19 12:32:35 -0800

    Levenshtein distance

--- epi_judge_cpp/levenshtein_distance.cc
+++ epi_judge_cpp/levenshtein_distance.cc
@@ -4,9 +4,33 @@
 using std::string;
 using std::vector;
 
+
 int LevenshteinDistance(const string& A, const string& B) {
-  // TODO - you fill in here.
-  return 0;
+  int m = A.length();
+  int n = B.length();
+  vector<vector<int>> dp(m+1, vector<int>(n+1));
+
+  for(int i = 0; i <=m; i++) {
+    dp[i][0] = i;
+  }
+
+  for(int i = 0; i <=n; i++) {
+    dp[0][i] = i;
+  }
+
+  for (int i = 1; i <= m; i++) {
+    for (int j = 1; j <= n; j++) {
+      int removeWord1 = dp[i-1][j] + 1;
+      int removeWord2 = dp[i][j-1] + 1;
+      int removeBoth = dp[i-1][j-1];
+      if (A[i-1] != B[j-1]) {
+        removeBoth += 1;
+      }
+
+      dp[i][j] = std::min(removeWord1, std::min(removeWord2, removeBoth));
+    }
+  }
+  return dp[m][n];
 }
 
 int main(int argc, char* argv[]) {
